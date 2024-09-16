5d929fbafb87d6265bf3f4ee1aa2e1c5b798cf24
Tolu Okunjoyo <toluokunjoyo@gmail.com>
2024-09-16 18:42:47 +0100

    resume uploade

--- app/util.ts
+++ app/util.ts
@@ -67,7 +67,7 @@ export const navContent: NavContentType[] = [
   {
     label: "Resume",
     //update at same resume
-    link: "https://drive.google.com/file/d/1k0zhy8TkAlNedSie21KcRlyJ9WZUsJ1O/view?usp=drivesdk",
+    link: "https://drive.google.com/file/d/1k0zhy8TkAlNedSie21KcRlyJ9WZUsJ1O/view?usp=sharing",
   },
 ];
 
